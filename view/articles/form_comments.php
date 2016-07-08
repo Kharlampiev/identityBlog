2f1cf72ac0822d6fa3ed1d4dfa992d151310f829
Kharley Dmitriy <lippogoo@gmail.com>
2016-07-08 11:54:49 +0300

    fixed style

--- view/articles/form_comments.php
+++ view/articles/form_comments.php
@@ -1,31 +1,28 @@
 <div class="row">
-	<div class="col-md-12">	
-			<div class="panel">	
+	<div class=" col-xs-12 col-md-12">	
+		<div class="panel">	
 			<div class="panel-body">	
-			<form class="form-horizontal comment-form" role="form" method="POST">
-			<div class="form-group">
-				<label  class="col-md-4 control-label"><em>Name</em> </label>
-				<div class="col-md-4">
-					<input type="text" name="name">
-				</div>
+				<form class="form-horizontal comment-form" role="form" method="POST">
+					<div class="form-group">
+						<label  class=" col-xs-2 col-md-4 control-label"><em>Name</em> </label>
+						<div class="col-xs-10 col-md-4">
+							<input type="text" name="name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label  class=" col-md-4 control-label"> <em>Comment</em></label>
+						<div class=" col-md-4">
+						<textarea class="form-control" name="text"> </textarea> 
+						</div>
+					</div>			
+					<div class="form-group">
+						<div class=" col-xs-12 col-md-4 col-md-offset-4">
+							<button type="submit" class="btn btn-primary">Send</button>
+						</div>
+					</div>
+				</form>
 			</div>
-			<div class="form-group">
-				<label  class="col-md-4 control-label"> <em>Comment text</em></label>
-				<div class="col-md-4">
-					<textarea class="form-control" name="text"> </textarea> 
-				</div>
-			</div>			
-			<div class="form-group">
-				<div class="col-md-4 col-md-offset-4">
-					<button type="submit" class="btn btn-primary">Send</button>
-				</div>
-			</div>
-		</form>
-			</div>
-
-
-			</div>
-		
+		</div>
 	</div>
 </div>
 
